dc18acdbfab83c694992d7d505757bab517d8643
Ron <ron@lisles.net>
2023-01-21 13:46:15 -0600

    Add watch app

---
+++ PatriotRVWatchApp/PatriotRVWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatriotRVWatchApp.swift
+//  PatriotRVWatch Watch App
+//
+//  Created by Ron Lisle on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PatriotRVWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
