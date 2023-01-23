0f3c52a67ad6f0031763698d1c655d940acf41c8
Ron <ron@lisles.net>
2023-01-23 06:41:49 -0600

    Add summary view

--- PatriotRV/Views/HomeView.swift
+++ PatriotRV/Views/HomeView.swift
@@ -11,30 +11,134 @@ struct HomeView: View {
 
     @EnvironmentObject var model: ModelData
     
+    @State private var showMenu = false
+    @State private var menuSelection: String? = nil
+    @State private var showCompleted = true
+    @State private var showPower = false    //TODO: remove
+
     var body: some View {
-        TabView {
+        NavigationStack {
             
-            ChecklistView()
-                //.badge()
-                .tabItem {
-                    Label("Checklist", systemImage: "list.clipboard")
+//            NavigationLink(
+//                destination: SettingsView().environmentObject(modelData),
+//                isActive: $showSettings) { EmptyView() }
+
+            GeometryReader { geometry in
+                
+                ZStack(alignment: .leading) {   // for sidemenu
+                    
+                    VStack {
+                        
+                        ZStack(alignment: .topLeading, content: {
+                            Image("truck-rv")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        })
+
+                        List {
+                            NavigationLink {
+                                PowerView()
+                            } label: {
+                                PowerRowView()
+                            }
+                            NavigationLink {
+                                ChecklistView()
+                            } label: {
+                                ChecklistRowView()
+                            }
+                            NavigationLink {
+                                LogView()
+                            } label: {
+                                LogRowView()
+                            }
+                        }//list
+                        .padding(.top, -8)
+                    }//vstack
+
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+                    .disabled(self.showMenu ? true : false)
+                    if self.showMenu {
+                        // Close side menu (This breaks onMove
+                        let drag = DragGesture()
+                            .onEnded {
+                                if $0.translation.width < -100 {
+                                    withAnimation {
+                                        self.showMenu = false
+                                    }
+                                }
+                            }
+
+                        MenuView(showMenu: $showMenu,
+                                 showCompleted: $showCompleted,
+                                 selection: $menuSelection, isShowingPower: $showPower)
+                            .frame(width: geometry.size.width/2)
+                            .transition(.move(edge: .leading))
+                        .gesture(drag)
+
+                    }
+
+                    
+                }//zstack
+                .navigationTitle("Summary")
+                .blackNavigation
+
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text("RV Checklist")
+                            .foregroundColor(.white)
+                    }
+                     ToolbarItem(placement: .navigationBarLeading) {
+                         Button(action: {
+                             withAnimation {
+                                 self.showMenu.toggle()
+                             }
+                         }) {
+                             Image(systemName: "line.horizontal.3")
+                                 .imageScale(.large)
+                         }
+                         .foregroundColor(.white)
+                     }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            withAnimation {
+                                self.showPower.toggle()
+                            }
+                        }) {
+                            Image(systemName: "gauge.low")
+                                .imageScale(.large)
+                        }
+                        .foregroundColor(.white)
+                    }
                 }
-            
-            PowerView()
-            //.badge()
-            .tabItem {
-                Label("Power", systemImage: "bolt.fill")
-            }
 
-            LogView()
-            //.badge()
-            .tabItem {
-                Label("Log", systemImage: "list.dash")
-            }
+                
+            }//geometryreader
+        }//navigationstack
+    }//body
+}
 
+struct PowerRowView: View {
+    var body: some View {
+        VStack {
+            Text("Power")
+            RvPowerView()
+            TeslaPowerView()
         }
-        .navigationTitle("Patriot RV")
-        .blackNavigation
+    }
+}
+
+struct ChecklistRowView: View {
+    var body: some View {
+        VStack {
+            Text("Checklist")
+        }
+    }
+}
+
+struct LogRowView: View {
+    var body: some View {
+        Text("Log")
     }
 }
 
