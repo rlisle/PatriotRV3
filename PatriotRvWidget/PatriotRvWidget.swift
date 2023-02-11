c2227c5fc561a982874454723fc997c74cb41793
Ron <ron@lisles.net>
2023-02-11 05:31:36 -0600

    wip

--- PatriotRvWidget/PatriotRvWidget.swift
+++ PatriotRvWidget/PatriotRvWidget.swift
@@ -10,34 +10,37 @@ import SwiftUI
 import Intents
 
 struct Provider: IntentTimelineProvider {
+    
     func placeholder(in context: Context) -> ChecklistEntry {
-        ChecklistEntry(date: Date(), configuration: ConfigurationIntent(), nextTrip: "Canada", nextItem: "Plan Trip")
+        ChecklistEntry(date: Date(),
+//                       configuration: ConfigurationIntent(),
+                       nextTrip: "Who knows?",
+                       nextItem: "Choose destination")
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (ChecklistEntry) -> ()) {
-        let entry = ChecklistEntry(date: Date(), configuration: configuration, nextTrip: "Canada", nextItem: "Plan Trip")
+        let entry = ChecklistEntry(date: Date(),
+//                                   configuration: configuration,
+                                   nextTrip: "Canada",
+                                   nextItem: "Plan Trip")
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [ChecklistEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = ChecklistEntry(date: entryDate, configuration: configuration, nextTrip: "Rockport", nextItem: "Check Roof")
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let entries = [
+            ChecklistEntry(date: Date(),
+                           nextTrip: "Rockport",
+                           nextItem: "Plan Trip"
+                          )
+            ]
+        let timeline = Timeline(entries: entries, policy: .never)
         completion(timeline)
     }
 }
 
 struct ChecklistEntry: TimelineEntry {
     var date: Date
-    let configuration: ConfigurationIntent
+    //let configuration: ConfigurationIntent
     let nextTrip: String
     let nextItem: String
 }
@@ -68,7 +71,12 @@ struct ChecklistWidget: Widget {
 
 struct ChecklistWidget_Previews: PreviewProvider {
     static var previews: some View {
-        ChecklistWidgetEntryView(entry: ChecklistEntry(date: Date(), configuration: ConfigurationIntent(), nextTrip: "Canada", nextItem: "Plan Trip"))
+        ChecklistWidgetEntryView(
+            entry: ChecklistEntry(
+                date: Date(),
+                //configuration: ConfigurationIntent(),
+                nextTrip: "Canada",
+                nextItem: "Plan Trip"))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
